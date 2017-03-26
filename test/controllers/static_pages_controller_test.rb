c3608cde70467a9a0b8ec786b9231406027b4e97
Jamie Locatis <jlocatis@gmail.com>
2017-03-26 10:27:13 -0500

    More tutorial stuff

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,13 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "To-Do on Rails"
+    assert_select "title", "Help | To Do on Rails"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "To Do on Rails"
+    assert_select "title", "About | To Do on Rails"
   end
 
 end
